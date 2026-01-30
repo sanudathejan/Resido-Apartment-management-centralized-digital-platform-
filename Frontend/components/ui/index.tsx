2187120f583b64bdd39434e4cd852de8ddd6e08b
sanudathejan <thejansanuda2006@gmail.com>
2026-01-30 22:15:07 +0530

    Create index.tsx

---
+++ Frontend/components/ui/index.tsx
@@ -0,0 +1,15 @@
+/**
+ * UI Components Index
+ * Export all reusable UI components
+ */
+
+export { default as GradientBackground } from './GradientBackground';
+export { default as Button } from './Button';
+export { default as InputField } from './InputField';
+export { default as DashboardCard } from './DashboardCard';
+export { default as SOSButton } from './SOSButton';
+export { default as Card } from './Card';
+export { default as Avatar } from './Avatar';
+export { default as StatusBadge } from './StatusBadge';
+export { default as EmptyState } from './EmptyState';
+export { default as LoadingSpinner } from './LoadingSpinner';
