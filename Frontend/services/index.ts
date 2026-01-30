fb27c226c1b3da0c64871f7831253fc193ffe40b
sanudathejan <thejansanuda2006@gmail.com>
2026-01-30 22:15:56 +0530

    Create index.ts

---
+++ Frontend/services/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Export all services
+ */
+
+export { default as apiService } from './api';
+export { default as authService } from './authService';
+export { default as paymentService } from './paymentService';
+export { default as maintenanceService } from './maintenanceService';
+export { default as parkingService } from './parkingService';
+export { default as sosService } from './sosService';
+export { default as announcementService } from './announcementService';
