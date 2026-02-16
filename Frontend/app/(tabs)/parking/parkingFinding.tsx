f11818b48c3619ec698ee0110c751cf74d3bf0e1
sani0309 <sanithiamalja25@gmail.com>
2026-02-16 11:23:50 +0530

    import block
    This section is the Import Block. In React Native, don't get all tools by default; have to "invite" specific components into  file so the app knows how to render themReact is imported alone, but the others are inside curly braces. This is because React is the Default Export, while the others are Named Exports—essentially specific tools located inside the React Native library.

---
+++ Frontend/app/(tabs)/parking/parkingFinding.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+  SafeAreaView,
+  StatusBar,
+  Dimensions,
+} from 'react-native';
