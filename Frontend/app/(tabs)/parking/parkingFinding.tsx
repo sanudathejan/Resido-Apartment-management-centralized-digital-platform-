cfa72bab102962796266499e4a86c5ad0b40c82f
sani0309 <sanithiamalja25@gmail.com>
2026-02-16 14:32:19 +0530

    using lucide icons for  a clean, modern look similar to the UI
    These two lines are about adding visuals and handling screen sizing.

--- Frontend/app/(tabs)/parking/parkingFinding.tsx
+++ Frontend/app/(tabs)/parking/parkingFinding.tsx
@@ -10,3 +10,7 @@ import {
   StatusBar,
   Dimensions,
 } from 'react-native';
+// Using Lucide icons for a clean, modern look similar to the UI
+import { Search, Grid, Phone, Home, Megaphone, SquareP, Banknote, UserCircle } from 'lucide-react-native';
+
+const { width } = Dimensions.get('window');
